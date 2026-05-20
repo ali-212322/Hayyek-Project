2ee09875fe5bd1839e6c347db51c1703ac34a7fa
Ali <alimalmaghrabi@gmail.com>
2026-05-21 02:57:59 +0300

    feat: add production deployment configuration for Render and Aiven

--- backend/hayyak/settings/prod.py
+++ backend/hayyak/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+from decouple import config
 
 DEBUG = False
 
@@ -11,3 +12,22 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Aiven MySQL مع SSL
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DB_NAME", default="defaultdb"),
+        "USER": config("DB_USER", default="avnadmin"),
+        "PASSWORD": config("DB_PASSWORD", default=""),
+        "HOST": config("DB_HOST", default=""),
+        "PORT": config("DB_PORT", default="19865"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "ssl": {
+                "ca": "/etc/ssl/ca.pem",
+            },
+        },
+    }
+}
